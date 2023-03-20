95cb71dd7617f7ee10c758d57a75b29bb2523907
Ms <qyg2297248353@163.com>
2023-03-20 17:57:52 +0800

    JPA

---
+++ ms-jpa/src/main/java/com/ms/jpa/model/service/impl/IBaseServiceImpl.java
@@ -0,0 +1,260 @@
+/*
+ * @MS 2022-12-13
+ * Copyright (c) 2001-2023 萌森
+ * 保留所有权利
+ * 本软件为萌森工作室所有及包含机密信息，须遵守其相关许可证条款进行使用。
+ * Copyright (c) 2001-2023 Meng Sen
+ * All rights reserved
+ * This software is owned by Mengsen Studio and contains confidential information, and must be used in accordance with its relevant license terms.
+ * Website：https://qyg2297248353.top
+ */
+
+package com.ms.jpa.model.service.impl;
+
+import com.ms.jpa.model.entity.IBaseEntity;
+import com.ms.jpa.model.mapper.IBaseRepository;
+import com.ms.jpa.model.service.IBaseService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.transaction.Transactional;
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * 基础服务实现类
+ *
+ * @author ms2297248353
+ */
+@Transactional
+public abstract class IBaseServiceImpl<T extends IBaseEntity, I extends Serializable> implements IBaseService<T, I> {
+    /**
+     * @return IBaseDao
+     */
+    public abstract IBaseRepository<T, I> getBaseRepository();
+
+    /**
+     * findById.
+     *
+     * @param id id
+     * @return T
+     */
+    @Override
+    public Optional<T> find(I id) {
+        return getBaseRepository().findById(id);
+    }
+
+    /**
+     * @return List
+     */
+    @Override
+    public List<T> findAll() {
+        return getBaseRepository().findAll();
+    }
+
+    /**
+     * @param ids ids
+     * @return List
+     */
+    @Override
+    public List<T> findList(I[] ids) {
+        List<I> idList = Arrays.asList(ids);
+        return getBaseRepository().findAllById(idList);
+    }
+
+    /**
+     * find list.
+     *
+     * @param spec spec
+     * @return list
+     */
+    @Override
+    public List<T> findList(Specification<T> spec) {
+        return getBaseRepository().findAll(spec);
+    }
+
+    /**
+     * find list.
+     *
+     * @param spec spec
+     * @param sort sort
+     * @return List
+     */
+    @Override
+    public List<T> findList(Specification<T> spec, Sort sort) {
+        return getBaseRepository().findAll(spec, sort);
+    }
+
+    /**
+     * find one.
+     *
+     * @param spec spec
+     * @return T
+     */
+    @Override
+    public T findOne(Specification<T> spec) {
+        return getBaseRepository().findOne(spec).orElse(null);
+    }
+
+    /**
+     * @param pageable pageable
+     * @return Page
+     */
+    @Override
+    public Page<T> findAll(Pageable pageable) {
+        return getBaseRepository().findAll(pageable);
+    }
+
+    /**
+     * count.
+     *
+     * @return long
+     */
+    @Override
+    public long count() {
+        return getBaseRepository().count();
+    }
+
+    /**
+     * count.
+     *
+     * @param spec spec
+     * @return long
+     */
+    @Override
+    public long count(Specification<T> spec) {
+        return getBaseRepository().count(spec);
+    }
+
+    /**
+     * exists.
+     *
+     * @param id id
+     * @return boolean
+     */
+    @Override
+    public boolean exists(I id) {
+        return getBaseRepository().findById(id).isPresent();
+    }
+
+    /**
+     * save.
+     *
+     * @param entity entity
+     */
+    @Override
+    public void save(T entity) {
+        getBaseRepository().save(entity);
+    }
+
+    /**
+     * save.
+     *
+     * @param entities entities
+     */
+    @Override
+    public void save(List<T> entities) {
+        getBaseRepository().saveAll(entities);
+    }
+
+    /**
+     * update.
+     *
+     * @param entity entity
+     * @return T
+     */
+    @Override
+    public T update(T entity) {
+        return getBaseRepository().saveAndFlush(entity);
+    }
+
+    /**
+     * delete.
+     *
+     * @param id id
+     */
+    @Override
+    public void delete(I id) {
+        getBaseRepository().deleteById(id);
+    }
+
+    /**
+     * delete by ids.
+     *
+     * @param ids ids
+     */
+    @Override
+    public void deleteByIds(List<I> ids) {
+        ids.forEach(i -> getBaseRepository().deleteById(i));
+    }
+
+    /**
+     * delete all.
+     */
+    @Override
+    public void deleteAll() {
+        getBaseRepository().deleteAllInBatch();
+    }
+
+    /**
+     * delete.
+     *
+     * @param entities entities
+     */
+    @Override
+    public void delete(T[] entities) {
+        List<T> tList = Arrays.asList(entities);
+        getBaseRepository().deleteAll(tList);
+    }
+
+    /**
+     * delete.
+     *
+     * @param entities entities
+     */
+    @Override
+    public void delete(Iterable<T> entities) {
+        getBaseRepository().deleteAll(entities);
+    }
+
+    /**
+     * delete.
+     *
+     * @param entity entity
+     */
+    @Override
+    public void delete(T entity) {
+        getBaseRepository().delete(entity);
+    }
+
+    /**
+     * @param ids ids
+     * @return List
+     */
+    @Override
+    public List<T> findList(Iterable<I> ids) {
+        return getBaseRepository().findAllById(ids);
+    }
+
+    /**
+     * @param spec     spec
+     * @param pageable pageable
+     * @return Page
+     */
+    @Override
+    public Page<T> findAll(Specification<T> spec, Pageable pageable) {
+        return getBaseRepository().findAll(spec, pageable);
+    }
+
+    /**
+     * flush.
+     */
+    @Override
+    public void flush() {
+        getBaseRepository().flush();
+    }
+}
