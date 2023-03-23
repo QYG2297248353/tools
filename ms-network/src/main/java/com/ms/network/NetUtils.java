7ce04351bb949df94692e55a483a001eff62232c
Ms <qyg2297248353@163.com>
2023-03-23 12:20:55 +0800

    update

--- ms-network/src/main/java/com/ms/network/NetUtils.java
+++ ms-network/src/main/java/com/ms/network/NetUtils.java
@@ -1,3 +1,14 @@
+/*
+ * @MS 2022-12-13
+ * Copyright (c) 2001-2023 萌森
+ * 保留所有权利
+ * 本软件为萌森工作室所有及包含机密信息，须遵守其相关许可证条款进行使用。
+ * Copyright (c) 2001-2023 Meng Sen
+ * All rights reserved
+ * This software is owned by Mengsen Studio and contains confidential information, and must be used in accordance with its relevant license terms.
+ * Website：https://qyg2297248353.top
+ */
+
 package com.ms.network;
 
 import java.net.InetAddress;
